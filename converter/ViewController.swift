c9c7a311001197564eb33202e86ceee4d155ad96
vanashimko <vana.shimko@gmail.com>
2017-02-07 11:39:18 +0300

    Implement basic behavior

--- converter/ViewController.swift
+++ converter/ViewController.swift
@@ -8,8 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum Currency{
+    case USD, EUR, RUB
+    func getRate() -> Double {
+        switch self {
+        case .EUR: return 0.4866
+        case .USD: return 0.5216
+        case .RUB: return 30.8166
+        }
+    }
+}
 
+class ViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +31,42 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet weak var editBYN: UITextField!
+    @IBOutlet weak var labelRUB: UILabel!
+    @IBOutlet weak var labelUSD: UILabel!
+    @IBOutlet weak var labelEUR: UILabel!
+    
+    @IBAction func buttonConvertClick(_ sender: Any) {
+        var valueInBYN: Double?;
+        if (editBYN.text ?? "").isEmpty {
+            valueInBYN = 1.0
+        } else {
+            valueInBYN = Double(editBYN.text!)
+        }
+        if valueInBYN != nil {
+            viewRates(valueInBYN: valueInBYN!)
+        } else {
+            handleInvalidInput()
+        }
+    }
+    
+    func handleInvalidInput() {
+        editBYN.text = nil
+        viewRates(valueInBYN: 1.0)
+        showAlert()
+    }
+    
+    func showAlert(){
+        let alertController = UIAlertController(title: "Error", message: "Invalid input.", preferredStyle:.alert)
+        let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(defaultAction)
+        show(alertController, sender:self)
+    }
+    
+    func viewRates(valueInBYN: Double){
+        labelUSD.text = "\(Currency.USD): \(valueInBYN*Currency.USD.getRate())"
+        labelEUR.text = "\(Currency.EUR): \(valueInBYN*Currency.EUR.getRate())"
+        labelRUB.text = "\(Currency.RUB): \(valueInBYN*Currency.RUB.getRate())"
+    }
 }
 
